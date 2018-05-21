404a00d5e45635dce05c5304bee1650fa229c4c8
Baptiste Truchet <baptiste.truchet@gmail.com>
2018-05-21 15:44:35 +0200

    validations completed

--- app/models/body.rb
+++ app/models/body.rb
@@ -6,4 +6,5 @@ class Body < ApplicationRecord
   validates :price_per_day, presence: true, numericality: { :greater_than_or_equal_to => 0 }
   validates :sex, presence: true, inclusion: { in: ["M", "F"] }
   validates :location, presence: true, allow_blank: false
+  validates :title, presence: true, allow_blank: false
 end
