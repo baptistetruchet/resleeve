3394d60c2bf55a776c685683faccd4ac6145b305
Nicolas Schweitzer <n.schweitzer10@gmail.com>
2018-05-22 18:16:33 +0200

    finished calendar

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -3,34 +3,37 @@ import "flatpickr/dist/flatpickr.min.css";
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 // import 'json';
 
-const forbidates = document.querySelector("#forbidden-dates").value;
+const input = document.querySelector("#forbidden-dates");
+if (input) {
+  const forbidates = input.value;
 
-function formatDate(date) {
-    var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
+  function formatDate(date) {
+      var d = new Date(date),
+          month = '' + (d.getMonth() + 1),
+          day = '' + d.getDate(),
+          year = d.getFullYear();
 
-    if (month.length < 2) month = '0' + month;
-    if (day.length < 2) day = '0' + day;
+      if (month.length < 2) month = '0' + month;
+      if (day.length < 2) day = '0' + day;
 
-    return [year, month, day].join('-');
-}
+      return [year, month, day].join('-');
+  }
 
-const arrayForbid = [];
-forbidates.split('X').forEach((s) => {
-  let arr = s.split('|')
-  arrayForbid.push({
-    from: arr[0],
-    to: arr[1]
+  const arrayForbid = [];
+  forbidates.split('X').forEach((s) => {
+    let arr = s.split('|')
+    arrayForbid.push({
+      from: arr[0],
+      to: arr[1]
+    });
   });
-});
 
-console.log(arrayForbid);
+  console.log(arrayForbid);
 
-flatpickr("#range_start", {
-  altInput: true,
-  minDate: "today",
-  disable: arrayForbid,
-  plugins: [new rangePlugin({ input: "#range_end"})]
-});
+  flatpickr("#range_start", {
+    altInput: true,
+    minDate: "today",
+    disable: arrayForbid,
+    plugins: [new rangePlugin({ input: "#range_end"})]
+  });
+};
