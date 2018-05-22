e56a78dc467768a7f0419886b4d50a6a2c87cc61
Nicolas Schweitzer <n.schweitzer10@gmail.com>
2018-05-22 13:40:02 +0200

    controller reviews and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :bodies do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: [:new, :create] do
+      resources :reviews, only: [:new, :create]
+    end
   end
 
 end
