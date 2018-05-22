d41cf88fcf26bc3ab78d1da51179b4e195bddc86
Baptiste Truchet <baptiste.truchet@gmail.com>
2018-05-22 12:19:16 +0200

    dependencies on destroy

--- app/models/body.rb
+++ app/models/body.rb
@@ -1,6 +1,6 @@
 class Body < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
 
   validates :price_per_day, presence: true, numericality: { :greater_than_or_equal_to => 0 }
