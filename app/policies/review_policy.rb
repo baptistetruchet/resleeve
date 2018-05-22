15080e162e278b5e300a361e98bca960b67d2edb
Nicolas Schweitzer <n.schweitzer10@gmail.com>
2018-05-22 14:48:43 +0200

    authorisation for reviews are working

--- app/policies/review_policy.rb
+++ app/policies/review_policy.rb
@@ -1,19 +1,24 @@
 class ReviewPolicy < ApplicationPolicy
   def create?
-    return true
+    # (record.booking.reviews.any? && record.booking.user == user) || user.admin
+    if record.booking.reviews.first.nil?
+      record.booking.user == user || user.admin
+    else
+      false
+    end
   end
 
-  def edit?
-    record.user == user || user.admin
-  end
+  # def edit?
+  #   record.user == user || user.admin
+  # end
 
-  def update?
-    record.user == user || user.admin
-  end
+  # def update?
+  #   record.user == user || user.admin
+  # end
 
-  def destroy?
-    record.user == user || user.admin
-  end
+  # def destroy?
+  #   record.user == user || user.admin
+  # end
 
   class Scope < Scope
     def resolve
