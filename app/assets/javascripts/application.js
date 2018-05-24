18e639c69c10664dc38eb3694ad17c2389a4a278
Baptiste Boucher <baptiste.boucher@gmail.com>
2018-05-24 19:28:22 +0200

    change button add file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,6 @@
 //= require rails-ujs
 //= require_tree .
+const browse = document.getElementById("upload-btn");
+browse.addEventListener("click", (event) => {
+  document.getElementById('body_photo').click();
+});
