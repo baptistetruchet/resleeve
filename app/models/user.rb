fbe77e16a6a92007996e77c4cdb7fcf0a2d448bd
Shiraz Kittelberger-Buehlbecker <buehlbeckershiraz@gmail.com>
2018-05-25 13:13:18 +0200

    notif on new messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,9 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true, allow_blank: false
   validates :last_name, presence: true, allow_blank: false
+
+  def new_messages
+    Message.where(receiver: self).where(viewed: false).count
+    # messages.count { |msg| msg.receiver == self && msg.viewed? == false }
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_24_102659) do
+ActiveRecord::Schema.define(version: 2018_05_25_100644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2018_05_24_102659) do
     t.bigint "conversation_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "viewed", default: false
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
     t.index ["receiver_id"], name: "index_messages_on_receiver_id"
     t.index ["sender_id"], name: "index_messages_on_sender_id"
